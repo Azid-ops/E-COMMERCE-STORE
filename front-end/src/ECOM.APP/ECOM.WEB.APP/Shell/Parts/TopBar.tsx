3dd8d4896980f23b227890629695c7a246388d22
Azid-ops <azixace361@gmail.com>
2024-09-21 23:28:10 +0500

    Implemented French
    Implemented French and integrated with UI

--- front-end/src/ECOM.APP/ECOM.WEB.APP/Shell/Parts/TopBar.tsx
+++ front-end/src/ECOM.APP/ECOM.WEB.APP/Shell/Parts/TopBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BrandLogo from "../../../ECOM.WEB.UI/Images/Logo.png";
 import '../../../ECOM.WEB.UI/Styles/Buttons.css';
 import { useTranslation } from 'react-i18next';
+import { Dropdown } from 'primereact/dropdown';
 
 const TopBar = (props) => {
     const { t } = useTranslation();
@@ -10,6 +11,15 @@ const TopBar = (props) => {
             <div style={{background:"black",display:"flex"}}>
                 <img src={BrandLogo} style={{width:70, height:50}} alt="Website Logo"/>
                 <div style={{marginLeft:"auto",display:"flex"}}>
+                    <Dropdown 
+                        value={props.dropDownLanguage} 
+                        onChange={props.changeLanguage}
+                        options={props.languages} 
+                        optionLabel="name" 
+                        style={{margin:"8px 10px"}}
+                        placeholder="Select a language" 
+                        className="w-full md:w-14rem" 
+                    />
                     <button className="Ecom-Ecom-Web-UI-Styles-Buttons-GLobalButtons">
                         {t('Login')}
                     </button>
@@ -17,26 +27,6 @@ const TopBar = (props) => {
                     <button className="Ecom-Ecom-Web-UI-Styles-Buttons-GLobalButtons">
                         {t('Signup')}
                     </button>
-
-                    <div style={{display:"flex"}}>
-                        <p style={{color:"white",padding:"0 5px 0 0"}}>Choose Language:</p>
-                        <p style={{color:"white",padding:"0 5px 0 0"}}>English</p>
-                        <label className="label">
-                            <div className="toggle" 
-                                 onClick={props.changeLanguage}
-                                 style={{marginTop:"15px"}}>
-                                <input
-                                    className="toggle-state"
-                                    type="checkbox"
-                                    checked={props.currentLanguage === 'ar'}
-                                    readOnly
-                                />
-                                <div className="indicator"></div>
-                            </div>
-</label>
-
-                        <p style={{color:"white",padding:"0 5px 0 0"}}>Arabic</p>
-                    </div>
                 </div>
             </div>
         </section>
--- front-end/src/ECOM.APP/ECOM.WEB.VIEWMODEL/Shell/Parts/TopBarViewModel.tsx
+++ front-end/src/ECOM.APP/ECOM.WEB.VIEWMODEL/Shell/Parts/TopBarViewModel.tsx
@@ -1,27 +1,39 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopBar from '../../../ECOM.WEB.APP/Shell/Parts/TopBar.tsx';
 import { useTranslation } from 'react-i18next';
 
-
-const TopBarViewModel = () =>{
-
+const TopBarViewModel = () => {
     const { i18n } = useTranslation();
+
+    const [languages] = useState([
+        { name: 'English', code: 'en' },
+        { name: 'Arabic', code: 'ar' },
+        { name: 'French', code: 'fr' }
+    ]);
+
+    const [dropDownLanguage, setDropDownLanguage] = useState({ name: 'English', code: 'en' }); 
     const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
 
     const changeLanguage = (e) => {
-        e.preventDefault();
-        const newLanguage = currentLanguage === "en" ? "ar" : "en";
-        i18n.changeLanguage(newLanguage);
-        setCurrentLanguage(newLanguage);
+        const newLanguageCode = e.value.code; 
+        i18n.changeLanguage(newLanguageCode);
+        setCurrentLanguage(newLanguageCode);
+        setDropDownLanguage(e.value);
     };
 
     useEffect(() => {
-        setCurrentLanguage(i18n.language);
-    }, [i18n.language]);
-    
-    return(
-        <TopBar changeLanguage={changeLanguage} currentLanguage={currentLanguage}/>
-    )
-}
+        const selectedLanguage = languages.find(lang => lang.code === i18n.language);
+        setDropDownLanguage(selectedLanguage);
+    }, [i18n.language, languages]);
+
+    return (
+        <TopBar
+            changeLanguage={changeLanguage} 
+            currentLanguage={currentLanguage}
+            dropDownLanguage={dropDownLanguage}
+            languages={languages}
+        />
+    );
+};
 
 export default TopBarViewModel;
